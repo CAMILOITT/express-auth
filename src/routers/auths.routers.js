71a26f8a23d51d7016768da929e223b55f8cb2af
CAMILOITT <camilocamilo5@outlook.com>
2022-10-27 19:29:34 -0500

    add config

--- src/routers/auths.routers.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, register } from '../controllers/auth.controllers';
-
-const router = Router();
-
-router.post('/auth/register', register);
-router.post('/auth/login', login);
-
-export default router;
